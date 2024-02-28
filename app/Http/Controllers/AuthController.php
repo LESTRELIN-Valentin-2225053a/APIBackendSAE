2da0c074de90752cb07842b7586d64eccfafe3f3
Rackyrr <valentin.lestrelin@etu.univ-amu.fr>
2024-02-28 21:08:37 +0100

    Ajout erreur auth

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -6,42 +6,53 @@ use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
 use Illuminate\Validation\ValidationException;
 
 
 class AuthController extends Controller
 {
     public function register(Request $request){
-        $request->validate([
-            'name' => ['required'],
-            'email' => ['required', 'email' , 'unique:users'],
-            'password' => ['required' , 'min:8']
+        $validator = Validator::make($request->all(), [
+            'name' => 'required',
+            'email' => 'required|email|unique:users',
+            'password' => 'required|min:8'
         ]);
 
-        $user = User::create([
-            'name' => $request->input('name'),
-            'email' => $request->input('email'),
-            'password' => Hash::make($request->input('password')),
-        ]);
+        if ($validator->fails()) {
+            return response()->json(['error' => "Les données ne sont pas valides"], 409);
+        }
+        else {
+            $user = User::create([
+                'name' => $request->input('name'),
+                'email' => $request->input('email'),
+                'password' => Hash::make($request->input('password')),
+            ]);
 
-        Auth::login($user);
+            Auth::login($user);
 
-        return response()->json(['message' => 'Successfully registered and logged in']);
+            return response()->json(['user' => $user], 201);
+        }
     }
 
     public function login(Request $request){
-        $request->validate([
-            'email'=> ['required'],
-            'password'=>['required']
+        $validator = Validator::make($request->all(), [
+            'email' => 'required|email',
+            'password' => 'required'
         ]);
 
-        if(Auth::attempt($request->only('email', 'password'))){
-            $request->session()->regenerate();
-            return response()->json(Auth::user(), 200);
+        if ($validator->fails()) {
+            return response()->json(['error' => "Les données ne sont pas valides"], 401);
         }
-        throw ValidationException::withMessages([
-            'email'=>['The provided credentials are incorrect.']
-        ]);
+        else {
+            if (Auth::attempt($request->only('email', 'password'))) {
+                $request->session()->regenerate();
+                return response()->json(Auth::user(), 200);
+            }
+            else {
+                return response()->json(['error' => "Les données ne sont pas valides"], 401);
+            }
+        }
     }
 
     public function logout(Request $request){
